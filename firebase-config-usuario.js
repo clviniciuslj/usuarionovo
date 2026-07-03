ffb940d7077628cb23941715c2f2bc0c22d5c0a8
clviniciuslj <viniciusbmxbike@gmail.com>
2026-07-03 16:49:27 -0300

    Add files via upload

---
+++ firebase-config-usuario.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDum7FyxneVNe3JQkmxuTbxCMpJSJscwvM",
+  authDomain: "controle-laranjeiras.firebaseapp.com",
+  databaseURL: "https://controle-laranjeiras-default-rtdb.firebaseio.com",
+  projectId: "controle-laranjeiras",
+  storageBucket: "controle-laranjeiras.firebasestorage.app",
+  messagingSenderId: "884128857219",
+  appId: "1:884128857219:web:638a0a6836aa2563b9afb6"
+};
